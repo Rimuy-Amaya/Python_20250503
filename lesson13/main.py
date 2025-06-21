1221553ebc1fd39249537f5e5ede98f2e3f83198
amaya <irelandbluemoon@gmail.com>
2025-06-21 16:19:12 +0800

    0621

--- lesson13/main.py
+++ lesson13/main.py
@@ -4,13 +4,7 @@ st.title("我的第一個Streamlit App")
 
 st.write("這是一個簡單的Streamlit應用程式。")
 
-name = st.text_input("請輸入你的名字：")
+import pandas as pd
 
-if name:
-    st.write(f"你好, {name}!")
-
-number = st.slider("選擇一個數字", 0, 100, 50)
-st.write(f"你選擇的數字是: {number}")
-
-if st.button("點擊我"):
-    st.success("按鈕被點擊了！")
+df = pd.read_csv("taiwan.csv")
+st.dataframe(df)
